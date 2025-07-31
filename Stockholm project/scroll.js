c5afbd73a415dec5d8301eb65810aba9f17e0d07
Literallybas <baselmsaa@gmail.com>
2025-07-31 15:28:13 +0300

    Delete Stockholm project directory

--- Stockholm project/scroll.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const startBtn = document.querySelector('.start-btn');
-
-  startBtn.addEventListener('click', () => {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
-  });
-});
